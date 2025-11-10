5e13aa8d37a73107be70455b397c6234607f4b48
Hikaru616 <h.sakashita@no-w-here.jp>
2025-11-10 20:10:35 +0900

    11/10 学習記録

--- test5.py
+++ test5.py
@@ -8,22 +8,47 @@ students = [
     {'name': '渡辺', 'score': 88, 'subject': 'english'}
 ]
 
-def decorator(f):
-    def wrapper(*arg, **kwarg):
-        start = time.time()
-        v = f(*arg, **kwarg)
-        end = time.time()
-        print(f"実行時間: {end - start}")
+def time_measure(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        v = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"実行時間 : {end_time - start_time}")
         return v
     return wrapper
 
-@decorator
-def high_score():
-    result = {student["name"] : student["score"] for student in students if student["score"] > 80 and student["subject"] == "math"}
-    # print(f"数学80点以上{result}")
-    return result
+def filter_transform_func(data, condition_func, transform_func):
+    return [
+        transform_func(item)
+        for item in data if 
+        condition_func(item)
+    ]
 
-high_score()
+@time_measure
+def process_math_students(students):
+    math_condition = lambda student : student['score'] >= 80 and student['subject'] == "math"
+    transform_to_string = lambda student : f"{student['name']}さん : {student['score']}点"
+    return filter_transform_func(students, math_condition, transform_to_string)
     
+math_results = process_math_students(students)
+print(f"数学80点以上 : {math_results}")
+
+
+# def decorator(f):
+#     def wrapper():
+#         start = time.time()
+#         v = f()
+#         end = time.time()
+#         print(f"実行時間: {end - start}")
+#         return v
+#     return wrapper
+
+# @decorator
+# def high_score():
+#     result = {student["name"] : student["score"] for student in students if student["score"] > 80 and student["subject"] == "math"}
+#     print(f"数学80点以上{result}")
+#     return result
+
+# high_score()
 
         
